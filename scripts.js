a958dec2771dd229093909fd3a2f67eb9b90b0f9
Quaridy <quaridyshop@gmail.com>
2025-03-26 11:32:04 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 let currentImageIndex = 1;
-const totalImages = 5; // Change this to the total number of images you have
+const totalImages = 8; // Change this to the total number of images you have
 
 const imageElement = document.getElementById('image');
 const skipButton = document.getElementById('skipButton');
@@ -19,7 +19,9 @@ skipButton.addEventListener('click', () => {
 
 fullscreenButton.addEventListener('click', () => {
     if (!document.fullscreenElement) {
-        document.documentElement.requestFullscreen();
+        document.documentElement.requestFullscreen().catch(err => {
+            console.error(`Error attempting to enable full-screen mode: ${err.message}`);
+        });
     } else {
         if (document.exitFullscreen) {
             document.exitFullscreen();
